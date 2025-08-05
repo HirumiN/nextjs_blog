f567cf64194d2b04b95c37879712a48cdbd93d3f
HirumiN <hilminurullah3@gmail.com>
2025-08-06 04:33:25 +1200

    DETAIL content

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-images:{
-  dangerouslyAllowSVG: true,
-      remotePatterns: [
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
       {
         // Izinkan semua domain yang menggunakan protokol https
-        protocol: 'https',
-        hostname: '**', 
+        protocol: "https",
+        hostname: "**",
       },
       {
         // Izinkan juga semua domain yang menggunakan http (opsional, jika perlu)
-        protocol: 'http',
-        hostname: '**',
+        protocol: "http",
+        hostname: "**",
       },
     ],
-}
+  },
+  experimental: {
+    ppr: "incremental",
+  },
+  devIndicators: {
+    position: "bottom-right",
+  },
 };
 
 export default nextConfig;
--- sanity/lib/query.ts
+++ sanity/lib/query.ts
@@ -14,6 +14,32 @@ export const STARTUP_QUERY = defineQuery(
             bio,},
         views,
         category,
-        image
+        image,
     }`
 );
+
+export const STARTUP_BY_ID_QUERY = defineQuery(
+    `*[_type == "startup" && _id==$id][0] {
+        _id,
+        title,
+        slug,
+        description,
+        _createdAt,
+        author -> {
+            _id,
+            name,
+          username,
+            image,
+            bio,},
+        views,
+        category,
+        image,
+          pitch,
+    }
+    `)
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+    *[_type == "startup" && _id==$id][0] {
+        _id, views
+    }
+  `)
